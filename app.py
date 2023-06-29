6113ee7af6594035f52f4547bc8f09ea081b133a
praveen pandey <114177903+praveenpandey77@users.noreply.github.com>
2023-06-29 03:58:26 +0000

    pkl applied

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request
+import pickle
 
 app=Flask(__name__)
+tokenizer= pickle.load(open('models/cv.pkl', "rb"))
+model=pickle.load(open('models/clf.pkl', "rb"))
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
 def home():
-    text=""
-    if request.method=='POST':
-        text=request.form.get('email-content')
-    return render_template('index.html', text=text)
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    email_text=request.form.get('email_content')
+    tokenized_email=tokenizer.transform(email_text)
+    predictions=model.transform(tokenized_email)
+    predictions=1 if prediction==1 else -1
+    return render_template('index.html', predictions=predictions, email_text=email_text)
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', debug=True)
